fc5b10c0781c0a40ca375374902aad41e1d70367
chemitaro <iwasi_44@hotmail.com>
2023-03-29 12:11:50 +0900

    リクエストと読み上げの非同期処理実装

--- lib/voicevox_trial_7.py
+++ lib/voicevox_trial_7.py
@@ -3,10 +3,12 @@ import requests
 import wave
 import simpleaudio as sa
 import concurrent.futures
+import threading
 import os
 import io
+import time
 
-def split_text(text, delimiters=("。", "．", ". ", "？", "? ", "！", "! ", "　", "\n")):
+def split_text(text, delimiters=("。", "、", "．", ". ", "？", "? ", "！", "! ", "　", "\n")):
     result = [text]
     for delimiter in delimiters:
         temp = []
@@ -23,6 +25,7 @@ def split_text_and_filepaths(texts, directory='.'):
     return result
 
 def generate_wav(text, speaker=52, filepath='./audio.wav', silence_duration=0.1):
+    print("requesting...")
     host = '127.0.0.1'
     port = 50021
     params = (
@@ -40,6 +43,7 @@ def generate_wav(text, speaker=52, filepath='./audio.wav', silence_duration=0.1)
         params=params,
         data=json.dumps(response1.json())
     )
+    print("getting response!")
 
     with wave.open(io.BytesIO(response2.content), 'rb') as input_wave:
         framerate = input_wave.getframerate()
@@ -54,7 +58,23 @@ def generate_wav(text, speaker=52, filepath='./audio.wav', silence_duration=0.1)
             output_wave.setframerate(input_wave.getframerate())
             output_wave.writeframes(remaining_frames)
 
+def generate_wav_async(texts, speakers, filepaths):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        list(executor.map(generate_wav, texts, speakers, filepaths))
+
 def play_audio(filepath):
+    max_attempts = 100
+    attempts = 0
+
+    while not os.path.exists(filepath) and attempts < max_attempts:
+        print(f"seeking file... count: {attempts}")
+        time.sleep(0.2)
+        attempts += 1
+
+    if attempts == max_attempts:
+        print("エラー: 指定されたファイルが見つかりませんでした。")
+        return
+
     wave_obj = sa.WaveObject.from_wave_file(filepath)
     play_obj = wave_obj.play()
     play_obj.wait_done()
@@ -81,8 +101,9 @@ def text_to_voicevox(text, speaker=52):
     filepaths = [d['filepath'] for d in text_and_filepaths]
     speakers = [speaker] * len(text_and_filepaths)
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        list(executor.map(generate_wav, texts, speakers, filepaths))
+    threading.Thread(target=generate_wav_async, args=(texts, speakers, filepaths,)).start()
+
+    time.sleep(0.5)
 
     for audio_file_path in filepaths:
         play_audio(audio_file_path)
